24e854a76c39c609fd9dd456916834ca614e4abe
Long Phan <pvlong2000@gmail.com>
2021-03-06 14:04:46 +0400

    use select() to monitor sockets that are ready for connection

--- FTPServer.c
+++ FTPServer.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
+void serve_client(int client_fd, char **users_list, char **pass_list);
 int userExist(char *user_name, char **users_list, int len);
 int validPassword(char *pass, char **pass_list, int index);
 
@@ -56,109 +57,148 @@ int main()
 		return -1;
 	}
 
+	// select() preparation
+	fd_set full_fdset, ready_fdset;
+	FD_ZERO(&full_fdset);
+	FD_SET(server_fd, &full_fdset);
+
+	int max_fd = server_fd;
+
 	//4. accept
 	struct sockaddr_in client_address;				 //we to pass this to accept method to get client info
 	int client_address_len = sizeof(client_address); // accept also needs client_address length
 
-	char client_name[50];
+	// char client_name[50];
 
 	while (1)
 	{
-		int client_fd = accept(server_fd, (struct sockaddr *)&client_address, (socklen_t *)&client_address_len); // waiting for connections from a client
-		char message[100];
-
-		inet_ntop(AF_INET, &client_address.sin_addr, client_name, sizeof(client_name));
-
-		if (client_fd < 0)
+		ready_fdset = full_fdset;
+		if (select(max_fd + 1, &ready_fdset, NULL, NULL, NULL) < 0)
 		{
-			perror("Accept: ");
+			perror("Select");
 			return -1;
 		}
 
-		int pid = fork();
-		if (pid == 0)
+		for (int i = 0; i <= max_fd; i++)
 		{
-			char command[100];
-			char user_name[100];
-			char pass[100];
-			int index;
-			int set_user = 0; // 0 means user is not set and 1 means otherwise
-			int authenticate_user = 0; // 0 means user has not authenticated and 1 means otherwise
-
-			while (1)
+			if (FD_ISSET(i, &ready_fdset)) // check if the bit at i is set
 			{
-				memset(message, 0, sizeof(message));
-				if (recv(client_fd, message, sizeof(message) - 1, 0) > 0)
+				if (i == server_fd) // if the bit is used by server socket
 				{
-					if (strcmp(message, "bye") == 0)
-					{
-						break;
-					}
+					// int client_fd = accept(server_fd, (struct sockaddr *)&client_address, (socklen_t *)&client_address_len); // waiting for connections from a client
+					// inet_ntop(AF_INET, &client_address.sin_addr, client_name, sizeof(client_name));
 
-					strncpy(command, &message[0], 5);
-					command[5] = 0;
-					printf("Command from client: %s\n", command);
-					if (strncmp(command, "USER ", 5) == 0) // USER command
-					{														  
-						strncpy(user_name, &message[5], sizeof(message) - 5); // get username
-						index = userExist(user_name, users_list, 2);
-						if (index >= 0)
-						{ // if user exists
-							set_user = 1;
-							strcpy(message, "Username OK, password required");
-							send(client_fd, message, strlen(message), 0);
-						}
-						else
-						{
-							strcpy(message, "Username doesn't exist");
-							send(client_fd, message, strlen(message), 0);
-						}
-					}
-					else if (strncmp(command, "PASS ", 5) == 0) // PASS command
+					int client_fd = accept(server_fd, NULL, NULL);
+					if (client_fd < 0)
 					{
-						if (set_user == 1) // if user is set
-						{
-							strncpy(pass, &message[5], sizeof(message) - 5); // get password
-							if (validPassword(pass, pass_list, index) == 1) // if correct password or user has authenticated
-							{
-								authenticate_user = 1;
-								strcpy(message, "Authentication complete");
-								send(client_fd, message, strlen(message), 0);
-							}
-							else if (authenticate_user == 1) {
-								strcpy(message, "Already authenticated with user name and password");
-								send(client_fd, message, strlen(message), 0);
-							}
-							else {
-								strcpy(message, "Wrong Password");
-								send(client_fd, message, strlen(message), 0);
-							}
-						}
-						else
-						{
-							strcpy(message, "Set USER first");
-							send(client_fd, message, strlen(message), 0);
-						}
+						perror("Accept: ");
+						return -1;
 					}
-					else // if command not recognized
-					{
-						strcpy(message, "Command not recognized");
-						send(client_fd, message, strlen(message), 0);
-					}
+					FD_SET(client_fd, &full_fdset);
+
+					if (client_fd > max_fd)
+						max_fd = client_fd;
+				}
+				else
+				{
+					serve_client(i, users_list, pass_list);
+					FD_CLR(i, &full_fdset);
 				}
 			}
-
-			printf("Client disconnected...! \n");
-			close(client_fd);
 		}
 	}
-
 	close(server_fd);
 	//5. send/receive
 	//6. close the socket
 	return 0;
 }
 
+/**
+ * 
+*/
+void serve_client(int client_fd, char **users_list, char **pass_list)
+{
+	char message[100];
+	char command[100];
+	char user_name[100];
+	char pass[100];
+	int index;
+	int set_user = 0;		   // 0 means user is not set and 1 means otherwise
+	int authenticate_user = 0; // 0 means user has not authenticated and 1 means otherwise
+
+	while (1)
+	{
+		memset(message, 0, sizeof(message));
+		if (recv(client_fd, message, sizeof(message) - 1, 0) > 0)
+		{
+			if (strcmp(message, "bye") == 0)
+			{
+				return;
+			}
+
+			strncpy(command, &message[0], 5);
+			command[5] = 0;
+			printf("Command from client: %s\n", command);
+
+			if (strncmp(command, "USER ", 5) == 0) // USER command
+			{
+				strncpy(user_name, &message[5], sizeof(message) - 5); // get username
+				index = userExist(user_name, users_list, 2);
+				if (index >= 0)
+				{ // if user exists
+					set_user = 1;
+					strcpy(message, "Username OK, password required");
+					send(client_fd, message, strlen(message), 0);
+				}
+				else
+				{
+					strcpy(message, "Username doesn't exist");
+					send(client_fd, message, strlen(message), 0);
+				}
+			}
+			else if (strncmp(command, "PASS ", 5) == 0) // PASS command
+			{
+				if (set_user == 1) // if user is set
+				{
+					strncpy(pass, &message[5], sizeof(message) - 5); // get password
+					if (validPassword(pass, pass_list, index) == 1)	 // if correct password or user has authenticated
+					{
+						authenticate_user = 1;
+						strcpy(message, "Authentication complete");
+						send(client_fd, message, strlen(message), 0);
+					}
+					else if (authenticate_user == 1)
+					{
+						strcpy(message, "Already authenticated with user name and password");
+						send(client_fd, message, strlen(message), 0);
+					}
+					else
+					{
+						strcpy(message, "Wrong Password");
+						send(client_fd, message, strlen(message), 0);
+					}
+				}
+				else
+				{
+					strcpy(message, "Set USER first");
+					send(client_fd, message, strlen(message), 0);
+				}
+			}
+			else // if command not recognized
+			{
+				strcpy(message, "Command not recognized");
+				send(client_fd, message, strlen(message), 0);
+			}
+		}
+	}
+
+	printf("Client disconnected...! \n");
+	close(client_fd);
+};
+
+/**
+ * 
+*/
 int userExist(char *user_name, char **users_list, int len)
 { // return index of user_name
 	int i;
@@ -172,6 +212,9 @@ int userExist(char *user_name, char **users_list, int len)
 	return -1; // if user name not found then return -1
 }
 
+/**
+ * 
+*/
 int validPassword(char *pass, char **pass_list, int index)
 { // check the pass of user name at an index
 	if (strcmp(pass_list[index], pass) == 0)
